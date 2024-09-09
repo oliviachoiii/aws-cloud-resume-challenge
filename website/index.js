fa11445cd16ce828a64155d2168f961dda9b39fd
oliviachoiii <oliviachoi612@gmail.com>
2024-09-08 23:18:01 -0700

    Update index.html/index.js

--- website/index.js
+++ website/index.js
@@ -26,9 +26,9 @@ function toggle_onclick($win, $navbar, width) {
 
 var typed = new Typed('#typed', {
     strings: [
-        'Cloud Engineer',
+        //'Cloud Engineer',
         //'Cloud Architect',
-        //'DevOps Engineer'
+        'DevOps Engineer'
     ],
     typeSpeed: 50,
     backSpeed: 50,
@@ -37,9 +37,9 @@ var typed = new Typed('#typed', {
 
 var typed_2 = new Typed('#typed_2', {
     strings: [
-        'Cloud Engineer',
+        //'Cloud Engineer',
         //'Cloud Architect',
-        //'DevOps Engineer'
+        'DevOps Engineer'
     ],
     typeSpeed: 50,
     backSpeed: 50,
